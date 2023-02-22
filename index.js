4d4cfe4fb718daf6c4f638719322680af6f44ce8
Gulp1n <abeldenouden@hotmail.com>
2023-02-22 21:46:19 +0100

    hihi it works now

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ let url = 'https://api.reddit.com/api/info/?id=t3_p9ak4n'
 const body = await fetch(url).then(res => res.json())
 const data = await body.data.children[0].data.selftext_html
 const html = await HtmlReplace(data)
-fs.writeFile('data.html', html, (err) => {
-    if (err) throw err;
-})
+// fs.writeFile('data.html', html, (err) => {
+//     if (err) throw err;
+// })
 const $ = cheerio.load(html)
+
+let pp = $("table")
+let pp2 = pp.first().text()
+if (pp2.includes('Hogwarts Legacy')){
+    console.log(':(')
+} else {
+    console.log(':)')
+}
